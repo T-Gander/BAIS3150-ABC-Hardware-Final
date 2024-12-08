9e66497be2d3a55d224cab0f8517a3efe2218d50
Thomas-Mac <thomas.gander99@gmail.com>
2024-12-07 23:14:44 -0700

    Sales technical services are complete?

--- TechnicalServices/Sales.cs
+++ TechnicalServices/Sales.cs
@@ -1,4 +1,8 @@
-﻿namespace BAIS3150_ABC_Hardware_Final.TechnicalServices
+﻿using BAIS3150_ABC_Hardware_Final.Domain;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace BAIS3150_ABC_Hardware_Final.TechnicalServices
 {
     public class Sales
     {
@@ -11,6 +15,82 @@
             DatabaseUsersBuilder.AddJsonFile("appsettings.json");
             IConfiguration DatabaseUsersConfiguration = DatabaseUsersBuilder.Build();
             _ABCSystemDB = DatabaseUsersConfiguration.GetConnectionString("NoSecurity");
+        }
+
+        public bool AddSale(Sale sale)
+        {
+            bool success = false;
+
+            try
+            {
+                using (SqlConnection Connection = new(_ABCSystemDB))
+                {
+                    SqlCommand AddSaleCommand = new()
+                    {
+                        Connection = Connection,
+                        CommandText = "AddSale",
+                        CommandType = CommandType.StoredProcedure
+                    };
+
+                    SqlParameter SaleDateParameter = new()
+                    {
+                        ParameterName = "@SaleDate",
+                        SqlDbType = SqlDbType.Date,
+                        SqlValue = sale.SaleDate
+                    };
+                    AddSaleCommand.Parameters.Add(SaleDateParameter);
+
+                    SqlParameter SalespersonIDParameter = new()
+                    {
+                        ParameterName = "@SalespersonID",
+                        SqlDbType = SqlDbType.Int,
+                        SqlValue = sale.SalespersonID
+                    };
+                    AddSaleCommand.Parameters.Add(SalespersonIDParameter);
+
+                    SqlParameter CustomerIDParameter = new()
+                    {
+                        ParameterName = "@CustomerID",
+                        SqlDbType = SqlDbType.Int,
+                        SqlValue = sale.CustomerID
+                    };
+                    AddSaleCommand.Parameters.Add(CustomerIDParameter);
+
+                    SqlParameter SubtotalParameter = new()
+                    {
+                        ParameterName = "@Subtotal",
+                        SqlDbType = SqlDbType.Money,
+                        SqlValue = sale.Subtotal
+                    };
+                    AddSaleCommand.Parameters.Add(SubtotalParameter);
+
+                    SqlParameter GSTParameter = new()
+                    {
+                        ParameterName = "@GST",
+                        SqlDbType = SqlDbType.Money,
+                        SqlValue = sale.GST
+                    };
+                    AddSaleCommand.Parameters.Add(GSTParameter);
+
+                    SqlParameter SaleTotalParameter = new()
+                    {
+                        ParameterName = "@SaleTotal",
+                        SqlDbType = SqlDbType.Money,
+                        SqlValue = sale.SaleTotal
+                    };
+                    AddSaleCommand.Parameters.Add(SaleTotalParameter);
+                    
+                    AddSaleCommand.ExecuteNonQuery();
+
+                    success = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                success = false;
+                Console.WriteLine(ex.ToString());
+            }
+            return success;
         }
     }
 }
